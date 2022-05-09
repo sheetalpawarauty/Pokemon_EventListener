00ac3fd695244b07d0ed1a48d18c22cc1eba79b2
SheetalPawarAuty <102964955+SheetalPawarAuty@users.noreply.github.com>
2022-05-09 11:22:24 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: 'pikachu', height: 4, types: ['field','fairy'] },
+    { name: 'bulbasaur', height: 7, types: ['monster','grass'] },
+    { name: 'paras', height: 3, types: ['bug','grass'] },
+    { name: 'seel', height: 11, types: ['water','field'] },
+    { name: 'squirtle', height: 5, types: ['monster','water'] }
+  ];
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+  function getAll() {
+    return pokemonList;
+  }
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+  }
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
+  };
+})();
+
+let pokemonGetList = pokemonRepository.getAll();
+pokemonGetList.forEach(function (pokemon){
+  pokemonRepository.addListItem(pokemon);
+});
